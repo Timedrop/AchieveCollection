ce42f47fb9ff9e986bcfa6c2698d362af97dcd24
Timedrop <timedrop.odin@gmail.com>
2014-10-24 22:57:07 +0900

    2010/10/24 GPSクラス作成 スピナー反応

--- src/jp/gr/java_conf/Timedrop/achievecollection/MainActivity.java
+++ src/jp/gr/java_conf/Timedrop/achievecollection/MainActivity.java
@@ -6,6 +6,11 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Toast;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 
 public class MainActivity extends Activity {
 
@@ -23,7 +28,19 @@ public class MainActivity extends Activity {
 		
 		spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
 			@Override
-		};
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+				String country = parent.getItemAtPosition(position).toString();
+				showToast("onItemSelected:" + country);
+			}
+			
+			@Override
+			public void onNothingSelected(AdapterView<?> parent){
+				showToast("onNothingSelected");
+			}
+		});
 	}
-
+	private void showToast(String text){
+		Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+	}
+	
 }
