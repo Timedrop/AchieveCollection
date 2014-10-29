0e83c6acb663b3b51ea5bbddf5c2a0bf39f5c5af
Timedrop <timedrop.odin@gmail.com>
2014-10-29 23:45:23 +0900

    2014/10/29 プロジェクト作り直し MainとGps作成 インテント間通信うまくいかず。

--- gen/jp/gr/java_conf/Timedrop/achievecollection/R.java
+++ gen/jp/gr/java_conf/Timedrop/achievecollection/R.java
@@ -29,16 +29,21 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int copy=0x7f070001;
-        public static final int flagItaly=0x7f070002;
-        public static final int spinner=0x7f070000;
+        public static final int copy=0x7f070002;
+        public static final int flagItaly=0x7f070003;
+        public static final int spinner=0x7f070001;
+        public static final int text_view_gps=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_gps=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int back=0x7f050005;
         public static final int copy=0x7f050002;
+        public static final int gps=0x7f050004;
+        public static final int text_view_gps=0x7f050003;
     }
 }
--- src/jp/gr/java_conf/Timedrop/achievecollection/MainActivity.java
+++ src/jp/gr/java_conf/Timedrop/achievecollection/MainActivity.java
@@ -2,26 +2,19 @@ package jp.gr.java_conf.Timedrop.achievecollection;
 
 import android.os.Bundle;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.View;
 import android.widget.Toast;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 
 public class MainActivity extends Activity {
-	LocationManager varLocationManager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		//LocationManagerクラスのインスタンスを取得
-		varLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 		
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.country_array,
 				android.R.layout.simple_spinner_item);
@@ -33,8 +26,8 @@ public class MainActivity extends Activity {
 		spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
 			@Override
 			public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
-				//String country = parent.getItemAtPosition(position).toString();
-				showToast(latitude);
+				String country = parent.getItemAtPosition(position).toString();
+				showToast("onItemSelected:" + country);
 			}
 			
 			@Override
@@ -43,46 +36,7 @@ public class MainActivity extends Activity {
 			}
 		});
 	}
-
-	@Override
-	//ユーザーからの操作の受付を開始するときに呼ばれる
-	protected void onResume()
-	{
-		super.onResume();
-		//現在の位置情報を更新
-		varLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 50, new SLocationListener());
-	}
-	
-	class SLocationListener implements LocationListener
-	{
-
-		@Override
-		//位置情報が更新されたときに呼び出されるコールバックメソッド
-		//public void onLocationChanged(Location lc)
-		{
-			//更新された経度を取得
-			int longitude = (int) (lc.getLongitude() * 1E6);
-			//更新された緯度を取得
-			int latitude = (int) (lc.getLatitude() * 1E6);
-			//座標を指定するためのGeoPointをインスタンス化
-			//GeoPoint varGeoPoint = new GeoPoint(latitude, longitude);
-		}
-
-		@Override
-		//LocationListenerで実装が必要
-		public void onProviderDisabled(String pv){
-		}
-		@Override
-		public void onProviderEnabled(String pv){
-		}
-
-		@Override
-		public void onStatusChanged(String pv, int status, Bundle ex){
-		}	
-	}
-	
 	private void showToast(String text){
 		Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
 	}
-	
 }
